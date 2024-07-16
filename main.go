43226f6b4c1edbdf0f0edf49c58b280b2e9a3b86
Hafiz Muhammad Fadli <48423387+hafizmfadli@users.noreply.github.com>
2024-07-16 13:04:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -31,6 +31,69 @@ func Quicksort(nums []int, start, end int) {
 
 }
 
+func Merge(arr, left, right []int) {
+	k := 0
+	posL := 0
+	posR := 0
+	lastIndexLeft := len(left) - 1
+	lastIndexRight := len(right) - 1
+	for {
+		if posL > lastIndexLeft || posR > lastIndexRight {
+			break
+		}
+
+		if left[posL] < right[posR] {
+			arr[k] = left[posL]
+			posL += 1
+		} else if right[posR] <= left[posL] {
+			arr[k] = right[posR]
+			posR += 1
+		}
+		k += 1
+	}
+
+	for {
+		if posL > lastIndexLeft {
+			break
+		}
+		arr[k] = left[posL]
+		k += 1
+		posL += 1
+	}
+
+	for {
+		if posR > lastIndexRight {
+			break
+		}
+		arr[k] = right[posR]
+		k += 1
+		posR += 1
+	}
+
+}
+
+func MergeSort(arr []int) {
+	if len(arr) < 2 {
+		return
+	}
+
+	mid := len(arr) / 2
+
+	left := make([]int, mid)
+	right := make([]int, len(arr)-mid)
+
+	for i := 0; i < mid; i++ {
+		left[i] = arr[i]
+	}
+	for i := mid; i < len(arr); i++ {
+		right[i-mid] = arr[i]
+	}
+
+	MergeSort(left)
+	MergeSort(right)
+	Merge(arr, left, right)
+}
+
 func main() {
 	nums := []int{8, 10, 2, 100, 21}
 	Quicksort(nums, 0, len(nums)-1)
